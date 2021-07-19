ccd848756ff29bacc6c5b3feeb5d5141e990a574
JoseDaniel959 <79713968+JoseDaniel959@users.noreply.github.com>
2021-07-19 15:36:44 -0500

    version finalizada

--- copiar_pegar.php
+++ copiar_pegar.php
@@ -1,28 +1,26 @@
 <?php
 function cortar($nombre,$ruta){
-    shell_exec("mv $nombre $ruta");
     echo 'ELemento fue cortado';
+    shell_exec("mv $nombre $ruta");
+    
 }
-
 function copiar($nombre,$ruta){
-    shell_exec("cp -r $nombre $ruta");
-    echo "Elemento fue copiado";
+   echo "Elemento fue copiado"; 
+   shell_exec("cp -r $nombre $ruta");
+   
 }
-
 if (isset($_POST['sendFiles'])){
-    $cont = json_decode($_POST['sendFiles'], true);
-
+    $cont = $_POST['sendFiles'];
+    var_dump($_POST["sendFiles"][0]["ruta_nombre"]);    
+    var_dump($cont);    
     foreach($cont as $files){
         if($files['hacer'] == 'copiar'){
             copiar($files['ruta_nombre'], $files['destino']);
         }
-        else if($files['hacer'] == 'cortat'){
+        else if($files['hacer'] == 'cortar'){
             cortar($files['ruta_nombre'], $files['destino']);
         }
         else echo "No hice nada :(";
     }
-
-}else echo "No tengo nada en POST"
-
-
+}else echo "No tengo nada en POST";
 ?>
