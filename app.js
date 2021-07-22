48e0c205c4ed462661566395da67c30b78c99a12
Juanpabloxv <juanpabloxv@hotmail.es>
2021-07-22 10:19:46 -0500

    cambios finales

--- app.js
+++ app.js
@@ -304,9 +304,9 @@ $(function(){
             $.post('copiar_pegar.php', {sendFiles}, (response) => {
                 console.log('Respuesta copiar y pegar')
                 console.log(response)
+                location.reload()
             })
 
-            location.reload()
         }
        
     })
